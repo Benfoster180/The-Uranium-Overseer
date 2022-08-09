148f45f5ed5315d9dfd5ad3f9b134f18436d4032
Cursed <31930426+Benfoster180@users.noreply.github.com>
2022-08-09 17:46:02 +0100

    Fixed Small Issue
    Issue would of prevented user reloading and loading files, via $load and $unload command!

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix="$")
 
 @bot.command()
 async def load(ctx):
-    if str(ctx.author.id) == "374830592140771330" or str(ctx.author.id) == "737907882200399875":
+    if str(ctx.author.id) == "Admin Discord ID":
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 bot.load_extension(f'cogs.{filename[:-3]}')
@@ -17,7 +17,7 @@ async def load(ctx):
 
 @bot.command()
 async def unload(ctx):
-    if str(ctx.author.id) == "374830592140771330" or str(ctx.author.id) == "737907882200399875":
+    if str(ctx.author.id) == "Admin Discord ID":
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 bot.unload_extension(f'cogs.{filename[:-3]}')
